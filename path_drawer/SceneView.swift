370af7aa5a930ff6afd956c9c2347ef3bdb5cbec
Darshan Patel <darshanpatel25894@gmail.com>
2017-09-28 10:10:27 -0400

    (SceneView) Added draw, touchBegin, touchMoved, touchEnd, dist functions

---
+++ path_drawer/SceneView.swift
@@ -0,0 +1,59 @@
+//
+//  SceneView.swift
+//  path_drawer
+//
+//  Created by DARSHAN DILIP PATEL on 9/28/17.
+//  Copyright © 2017 scratchwork. All rights reserved.
+//
+
+import UIKit
+
+
+
+class SceneView : UIView {
+
+    // tuple of points
+    var points = [CGPoint]();
+    var startPoint = CGPoint.zero;
+    
+    override func draw(_ rect: CGRect) {
+        for point in points {
+            let dot = UIBezierPath(ovalIn : CGRect(x : point.x-5, y : point.y-5, width : 10, height : 10));
+            UIColor.darkGray.setFill();
+            dot.fill();
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let currPoint = touch.location(in: self);
+            // checks if the current point is not equal to start point
+            if (startPoint != CGPoint.zero) {
+                startPoint = currPoint;
+            }
+            points.append(currPoint);
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let currPoint = touch.location(in: self);
+            points.append(currPoint);
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let currPoint = touch.location(in: self);
+            points.append(currPoint);
+            setNeedsDisplay();
+            
+        }
+        
+    }
+    
+    func euclidean_dist(start : CGPoint, end : CGPoint) -> Int {
+        let dist = sqrt(pow(end.x - start.x, 2) + pow(end.y - start.y, 2))
+        return Int(dist)
+    }
+}
