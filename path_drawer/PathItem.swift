80ea64ec3deed5e9217ca9b5fbe4a765d596c65f
Darshan Patel <darshanpatel25894@gmail.com>
2017-10-01 13:42:30 -0400

    (PathItem) Added PathItem File

--- path_drawer/PathItem.swift
+++ path_drawer/PathItem.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import UIKit
+
+class PathItem {
+    var points = [CGPoint]()
+    
+    init (pointsArr: CGPoint...) {
+        for point in pointsArr {
+            points.append(point)
+        }
+    }
+    
+    
+    func draw(_ rect: CGRect) {
+        for point in points {
+            let dot = UIBezierPath(ovalIn : CGRect(x : point.x-5, y : point.y-5, width : 10, height : 10));
+            UIColor.darkGray.setFill();
+            dot.fill();
+        }
+    }
+}
