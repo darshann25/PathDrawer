80be7e3c692c940da46b5bde190412908feeb6f3
Darshan Patel <darshanpatel25894@gmail.com>
2017-10-10 10:14:44 -0400

    (ItemState) Added initial code for ItemState

---
+++ path_drawer/state/itemstates/ItemState.swift
@@ -0,0 +1,28 @@
+//
+//  ItemState.swift
+//  path_drawer
+//
+//  Created by Darshan Patel on 10/10/17.
+//  Copyright © 2017 scratchwork. All rights reserved.
+//
+
+import Foundation
+
+class ItemState {
+    /*
+    public ItemType type = NSNull;
+    public var id = 0;
+    public var devId = 0;
+    public Matrix matrix;
+    
+    // abstract class ItemState
+    func ItemState(type : Item.Type, id : var, devId: var, matrix : Matrix) {
+        
+        self.type = type;  // corresponds to Item.types enum
+        self.id = id;
+        self.devId = devId;
+        self.matrix = matrix;
+        
+    }
+    */
+}
