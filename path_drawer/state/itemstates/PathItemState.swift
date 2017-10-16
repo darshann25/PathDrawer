94713f85e4f04d0dfe5d37ceaba5315b7ab9e421
Darshan Patel <darshanpatel25894@gmail.com>
2017-10-16 19:41:08 -0400

    (PathItemState) Basic porting of class from website with no dependencies resolved

--- path_drawer/state/itemstates/PathItemState.swift
+++ path_drawer/state/itemstates/PathItemState.swift
@@ -7,3 +7,67 @@
 //
 
 import Foundation
+
+// PathItemState inherits from ItemState
+class PathItemState : ItemState {
+    
+    /*
+    // private variables to PathItemState
+    var resource;
+    var beginIndex;
+    var endIndex;
+    var color;
+    var size;
+    var opacity;
+    
+    func PathItemState(id : var, devId : var, matrix : Matrix, resource : var, beginIndex : var, endIndex : var, color : var, size : var, opacity : var) {
+        
+        super(ItemType.Path, id, devId, matrix);
+        self.resource = resource;
+        self.beginIndex = beginIndex;
+        self.endIndex = endIndex;
+        self.color = color;
+        self.size = size;
+        self.opacity = opacity;
+    }
+    
+    /*
+     PathItemState.prototype = Object.create(ItemState.prototype);
+     PathItemState.prototype.constructor = PathItemState;
+     PathItemState.prototype.minify = function() {
+         return {
+             version: 1, // TODO: use a constant
+             itemType: Item.types.PathItem,
+             id: this.id,
+             devId: this.devId,
+             matrix: this.matrix.toArray(),
+             resource: {
+             id: this.resource.id,
+             devId: this.resource.devId,
+         },
+         beginIndex: ('beginIndex' in this ? this.beginIndex : 0),
+         endIndex: ('endIndex' in this ? this.endIndex : this.resource.data[0].length - 1),
+         color: this.color,
+         size: this.size,
+         opacity: this.opacity,
+         };
+     };
+    */
+    
+    // protected (invoke ItemState.unminify outside this file)
+    internal func unminify(mini : ItemState) {
+        var id = mini.id;
+        var matrix = Matrix.fromArray(mini.matrix);
+        var resourceId = mini.resource.id;
+        var resourceDevId = mini.resource.devId;
+        var resource = boardStateManager.getResource(resourceId, resourceDevId);
+        // var beginIndex = ('beginIndex' in mini ? mini.beginIndex : 0);
+        // var endIndex = ('endIndex' in mini ? mini.endIndex : resource.data[0].length - 1);
+        var color = mini.color;
+        var size = mini.size;
+        var opacity = mini.opacity;
+        return PathItemState(id, mini.devId, matrix, resource, beginIndex, endIndex, color, size, opacity);
+
+    }
+     */
+}
