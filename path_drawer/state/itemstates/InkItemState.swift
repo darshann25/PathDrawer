9041c264dcfa1f5567082cba6fb0f9b9ececfe64
Darshan Patel <darshanpatel25894@gmail.com>
2017-10-16 19:40:30 -0400

    (InkItemState) Basic porting of class from website with no dependencies resolved

--- path_drawer/state/itemstates/InkItemState.swift
+++ path_drawer/state/itemstates/InkItemState.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+class InkItemState : ItemState {
+    
+    /*
+     var resource;
+     
+     func InkItemState(id : var, devId : var, matrix : Matrix, resource : var) {
+         super(ItemType.Image, id, devId, matrix);
+         self.resource = resource;
+     }
+     
+     // TODO : Understand what minify  does and port it from website
+     //ImageItemState.prototype = Object.create(ItemState.prototype);
+     //ImageItemState.prototype.constructor = ImageItemState;
+     /*
+     ImageItemState.prototype.minify = function() {
+         return {
+             version: 1, // TODO: use a constant
+             itemType: Item.types.ImageItem,
+             id: this.id,
+             devId: this.devId,
+             matrix: this.matrix.toArray(),
+             resource: {
+             id: this.resource.id,
+             devId: this.resource.devId,
+         },
+     }; */
+     
+     // protected (invoke ItemState.unminify outside this file)
+     internal func unminify(mini : ItemState) {
+         var id = mini.id;
+         var matrix = Matrix.fromArray(mini.matrix);
+         var resourceId = mini.resource.id;
+         var resourceDevId = mini.resource.devId;
+         var resource = boardStateManager.getResource(resourceId, resourceDevId);
+         return InkItemState(id, mini.devId, matrix, resource);
+     }
+     */
+    
+}
+
