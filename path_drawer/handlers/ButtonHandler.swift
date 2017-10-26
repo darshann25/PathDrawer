6c183f748b80c6c181750a1995aadc61d8f9a605
Henry Stahl <henrystahl@Henrys-MacBook-Pro-2.local>
2017-10-25 20:13:33 -0400

    added tool manager, button handler, minor edits

---
+++ path_drawer/handlers/ButtonHandler.swift
@@ -0,0 +1,15 @@
+//
+//  ButtonHandlers.swift
+//  path_drawer
+//
+//  Created by Henry Stahl on 10/25/17.
+//  Copyright © 2017 scratchwork. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ButtonHandlers {
+
+    var test = 0
+}
---
+++ path_drawer/tools/ToolManager.swift
@@ -0,0 +1,15 @@
+//
+//  toolManager.swift
+//  path_drawer
+//
+//  Created by Henry Stahl on 10/25/17.
+//  Copyright © 2017 scratchwork. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ToolManager {
+    
+    var penTool = PenTool()
+}
