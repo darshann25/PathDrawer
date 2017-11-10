cb4c8f24312df0f92ef5076630c39327a965720b
Darshan Patel <darshanpatel25894@gmail.com>
2017-11-10 00:36:45 -0500

    (Messenger) Added stubs for singleton use. [PRIORITY 4]

--- path_drawer/Messenger.swift
+++ path_drawer/Messenger.swift
@@ -1,13 +1,62 @@
-//
-//  Messenger.swift
-//  path_drawer
-//
-//  Created by Henry Stahl on 11/7/17.
-//  Copyright © 2017 scratchwork. All rights reserved.
-//
+/*
+ The messenger singleton is responsible for sending and reporting messages.
+ 
+ // send a message to all devices
+ messenger.broadcast(type,message)
+ 
+ // receive messages for a given type
+ //   (handlerFn is of the form function(message,from){...} )
+ messenger.onmessage(type,handlerFn)
+ 
+ */
 
 import Foundation
 
 class Messenger {
+    
+    // list of all message handlers
+    var handlers : Dictionary<String, Any>;
+    
+    // used to save messages received before a device instance is created (indexed by deviceId)
+    var inboxes : Dictionary<String, Any>;
+    
+    init() {
+        handlers = [String: Any]();
+        inboxes = [String: Any]();
+    }
+    
+    // sends message to a list of devices through the server
+    func sendMessageViaServer(type : String, message : String, to : String) {
+        
+    }
+    
+    // similar to broadcast, but only to another device
+    //   TODO rename to sendMessageToDevice
+    func sendMessageTo(type : String, message : String, deviceId : String) {
+    
+    }
+
+    // sends message to all other devices as efficiently as possible
+    func broadcast(type : String, message : String) {
+    
+    }
+    
+    func onmessage(type: String, f : String) {
+        handlers[type] = f;
+    }
+    
+    // internal, delivers to the appropriate handler (at this point, the device instance does exist)
+    func deliverMessage(type : String, message : String, from : String) {
+    
+    }
+    
+    func incomingMessage(type : String, message : String, from : String) {
+    // TODO handle case where device created, but message queue not empty (concurrency issue not actually relevant in javascript, right?)
+    
+    }
+    
+    func releaseMessagesFromDeviceWithId(deviceId : String) {
+    
+    }
     
 }
--- path_drawer/tools/MagicEraserTool.swift
+++ path_drawer/tools/MagicEraserTool.swift
@@ -7,59 +7,88 @@
 //
 
 import Foundation
+import UIKit
 
 /*
  * MagicEraserTool is responsible for removing entire items that are hit by the mouse drag.
  */
 
 class MagicEraserTool : Tool {
-    /*
     // Used to determine the swipe segment, which is used to locate any items.
-    var previousPoint;
-    var actId = null;
+    var previousPoint : Point;
+    var actId : Any;
     
-    func onDown() {
-        previousPoint = [CGPoint]();
-        actId = null;
+    init(point : Point, id : Int) {
+        self.previousPoint = point;
+        self.actId = id;
     }
     
-    func onDrag() {
-        var point = CGPoint.zero;
-        var items = scene.getItemsIntersectingSegment(point, previousPoint);
-        if (items.length > 0) {
-            if (!actId) {
-                actId = boardStateManager.getNewActId();
+    func onDown(x : Double, y : Double) {
+        self.previousPoint = Point(x : x, y : y);
+        actId = -1;
+    }
+    
+    func onDrag(_x : Double, _y : Double) {
+        let point = Point(x : _x, y : _y);
+        var items = sceneView.scene.getItemsIntersectingSegment(end1: point, end2: self.previousPoint);
+        
+        if(items.count > 0) {
+            if(actId is Int) {
+                //self.actId = boardStateManager.getNewActId();
             }
-            var deviceId = devicesManager.getMyDeviceId();
-            scene.beginChanges();
-            items.forEach(function(item) {
-                var delta = DeleteItemDelta(actId,
-                                            deviceId,
-                                            item.state);
-                boardStateManager.addDelta(delta);
-                devicesManager.enqueueDelta(delta);
-                delta.applyToScene(); // scene.removeSceneItem(item);
-            });
-            scene.endChanges();
-            devicesManager.send();
         }
-        previousPoint = point;
     }
     
-    func onUp() {
-        // Just to be safe...
-        previousPoint = null;
-        actId = null;
-    }
-    
-    var public_interface = {
-        onDown: onDown;
-        onDrag: onDrag;
-        onUp: onUp,
-        func getCursor();
-    };
-    return public_interface;
-*/
+    /*
+     export default function MagicEraserTool() {
+     
+     // Used to determine the swipe segment, which is used to locate any items.
+     var previousPoint;
+     var actId = null;
+     
+     function onDown(x, y) {
+     previousPoint = new Point(x, y);
+     actId = null;
+     }
+     
+     function onDrag(x, y) {
+     var point = new Point(x, y);
+     var items = scene.getItemsIntersectingSegment(point, previousPoint);
+     if (items.length > 0) {
+     if (!actId) {
+     actId = boardStateManager.getNewActId();
+     }
+     var deviceId = devicesManager.getMyDeviceId();
+     scene.beginChanges();
+     items.forEach(function(item) {
+     var delta = new DeleteItemDelta(actId,
+     deviceId,
+     item.state);
+     boardStateManager.addDelta(delta);
+     devicesManager.enqueueDelta(delta);
+     delta.applyToScene(); // scene.removeSceneItem(item);
+     });
+     scene.endChanges();
+     devicesManager.send();
+     }
+     previousPoint = point;
+     }
+     
+     function onUp() {
+     // Just to be safe...
+     previousPoint = null;
+     actId = null;
+     }
+     
+     var public_interface = {
+     onDown: onDown,
+     onDrag: onDrag,
+     onUp: onUp,
+     getCursor: function() { return 'default'; },
+     };
+     return public_interface;
+     }
+     */
 }
 
 
