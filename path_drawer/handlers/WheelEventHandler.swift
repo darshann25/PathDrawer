6c698844ef822dcbb6042cd3619934a2cf9b1698
Sai Reddy <saireddy@Sais-MacBook-Pro.local>
2017-11-10 21:40:38 -0500

    (ItemState, EventHandler) Added implementation for equationIS. Partially completeted imageItem, EventHandler. [PRIORITY 3]

--- path_drawer/handlers/WheelEventHandler.swift
+++ path_drawer/handlers/WheelEventHandler.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import UIKit
+
+class WheelEventHandler : UIView {  //It is not a class. It is a default function. It has to be changed to init()
+ /*
+    var canvas                //NOTE:  Declared in website.
+    var ctrlKey = false         //NOTE:  it is given a random type here (Also declaration not in website)
+    var deltaMode = false       //NOTE:  it is given a random type here (Also declaration not in website)
+    var deltaY : Int            //NOTE:  it is given a random type here (Also declaration not in website)
+    func onWheel( event : WheelEventHandler)
+    {
+        let ZOOM_DELTA_PIXELS_PER_LINE = 12
+        let PAN_DELTA_PIXELS_PER_LINE=18
+        
+        let ZOOM_SCALING_FACTOR = 0.01
+        
+        if(event.ctrlKey){
+            var dy = event.deltaY
+            
+            if(event.deltaMode == true)
+            {
+                dy = dy * ZOOM_DELTA_PIXELS_PER_LINE
+            }
+            sceneViewOnZoom(event , event.clientX,event.clientY,1 - dy * ZOOM_SCALING_FACTOR)
+        }
+        
+    }
+    */
+}
+
+
+
