886ab89dbcb6eb454caa7e816a6a2ad3f897eae8
Darshan Patel <darshanpatel25894@gmail.com>
2017-11-10 19:16:58 -0500

    (Peer, PeersManager) Added stubs and implementation for Peer and PeersManager [PRIORITY 4]

--- path_drawer/Peer.swift
+++ path_drawer/Peer.swift
@@ -10,4 +10,48 @@ import Foundation
 
 class Peer {
     
+    var peerId : Int;
+    var colorIndex : Int;
+    var color : String;
+    var name : String;
+    var devices : [Device];
+    var inCall : Bool;
+    var rejectedRecentCall : Bool;
+    
+    init(peerId : Int) {
+        self.peerId = peerId;
+        self.colorIndex = 0;
+        self.color = "grey";
+        self.name = "noname";
+        self.devices = [];
+        self.inCall = false; // default, but not necessarily true
+        self.rejectedRecentCall = true; // must be true by default, since a new Peer won't know about the call
+    }
+    
+    func addDevice(device : Device) {
+        self.devices.append(device);
+    }
+    
+    func removeDevice(device : Device) {
+        var index = -1;
+        for i in 0..<self.devices.count {
+            if (device === self.devices[i]) {
+                index = i;
+                break;
+            }
+        }
+        if (index >= 0) {
+            self.devices.remove(at: index);
+        }
+    }
+    
+    func countDevices() -> Int {
+        return self.devices.count;
+    };
+    
+    func onLeave() {
+        // empty function
+    }
+    
+    
 }
