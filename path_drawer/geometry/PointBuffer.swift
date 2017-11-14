feac0999c18740a2baa65dd64f08c98425e8c565
Darshan Patel <darshanpatel25894@gmail.com>
2017-11-14 16:13:01 -0500

    (PointBuffer) Finished implementation of PointBuffer - Dependency for PathItem [PRIORITY 1]

--- path_drawer/geometry/PointBuffer.swift
+++ path_drawer/geometry/PointBuffer.swift
@@ -8,7 +8,39 @@
 
 import Foundation
 class PointBuffer {
+    
+    private var points : [Point]
+    public var xs : [Double]
+    public var ys : [Double]
+    
     init(pointsArray : [Point]) {
+        self.points = pointsArray
+        
+        for i in 0...pointsArray.count {
+            xs[i] = pointsArray[i].x
+            ys[i] = pointsArray[i].y
+        }
         
     }
+    
+    // returns the point at parameter s (using linear interpolation if s is not an integer)
+    func getPoint(s : Double) -> Point {
+        
+        let fl_s = floor(s)
+        let lambda = s - fl_s
+        let x = (1 - lambda) * self.points[fl_s].x + lambda * self.points[fl_s + 1].x
+        let y = (1 - lambda) * self.points[fl_s].y + lambda * self.points[fl_s + 1].y
+        let pt = Point(x : x, y : y)
+        
+        return pt
+    }
+    
+    func toArray() {
+        return self.points;
+    }
+    
+    func fromArray() {
+        // TODO
+    }
+
 }
